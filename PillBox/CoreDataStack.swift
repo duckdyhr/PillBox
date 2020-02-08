25a42d81ff55346ae6b5824f8a7b40cf84e384f0
Anne Dyhr <annedyhr@gmail.com>
2020-02-08 16:21:59 +0100

    Changes so UIWindow relevant code is in SceneDelegate not AppDelegate

---
+++ PillBox/CoreDataStack.swift
@@ -0,0 +1,39 @@
+//
+//  CoreDataStack.swift
+//  PillBox
+//
+//  Created by Anne Dyhr on 08/02/2020.
+//  Copyright © 2020 duckdyhr Inc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+func createMainContext() -> NSManagedObjectContext {
+    // Initialize NSManagedObjectsModel
+    let modelURL = Bundle.main.url(forResource: "PillBox", withExtension: "momd")
+    guard let model = NSManagedObjectModel(contentsOf: modelURL!) else { fatalError("model not found") }
+    
+    // Configure NSPersistentStoreCoordinator with an NSPersistentStore
+    let psc = NSPersistentStoreCoordinator(managedObjectModel: model)
+    let storeURL = URL.documentsURL.appendingPathComponent("PillBox.sqlite")
+    
+    // Create and return NSManagedObjectContext
+    try! psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+    let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    context.persistentStoreCoordinator = psc
+
+    return context
+}
+
+extension URL {
+    static var documentsURL: URL {
+        return try! FileManager
+            .default
+            .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    }
+}
+
+protocol ManagedObjectContextDependentType {
+    var managedObjectContext: NSManagedObjectContext! { get set}
+}
